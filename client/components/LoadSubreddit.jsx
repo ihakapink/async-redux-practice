b237164c28fa205443c8631298373eb702057498
Philip Ji <phil12p345@gmail.com>
2019-04-24 12:40:32 +1200

    add a button

--- client/components/LoadSubreddit.jsx
+++ client/components/LoadSubreddit.jsx
@@ -2,13 +2,36 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { fetchPosts } from '../actions'
 
-const LoadSubreddit = ({ children, dispatch }) => (
-  <div>
-    <button onClick={() => dispatch(fetchPosts('newzealand'))}>
-      Fetch Posts
-    </button>
-    {children}
-  </div>
-)
+class LoadSubreddit extends React.Component {
+  state = {
+    subReddit: ''
+  }
+
+handleChange = (e) => {
+  this.setState({
+    subReddit: e.target.value
+  })
+}
+
+render () {
+  return (
+    <form onChange={this.handleSubmit}>
+      <label>
+      Subreddit:
+        <input type="text" value={this.state.value} onChange={this.handle} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  )
+}
+}
+// const LoadSubreddit = ({ children, dispatch }) => (
+//   <div>
+//     <button onClick={() => dispatch(fetchPosts('newzealand'))}>
+//       Fetch Posts
+//     </button>
+//     {children}
+//   </div>
+// )
 
 export default connect()(LoadSubreddit)
